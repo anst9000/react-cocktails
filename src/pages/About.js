6e049d1474bb1936fc027485690eca3cd75a518f
Anders Strömberg <coltlaboy@yahoo.se>
2022-01-30 20:33:53 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis esse laborum pariatur nisi assumenda, consequuntur sequi minima sint consectetur, quae necessitatibus dolor asperiores omnis in eaque quos facere cum consequatur tenetur aperiam exercitationem ducimus? Cumque magni officiis eligendi reiciendis facilis.</p>
+    </section>
+  )
+}
+
+export default About
